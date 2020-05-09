a6e91b418ce8b4981e559e01e5bd67a5ebd8565a
noranuko13 <noranuko13@gmail.com>
2020-05-09 19:47:17 +0900

    :notes: [Shell Script] Add Oreore scripts.

---
+++ README.md
@@ -0,0 +1,7 @@
+# cathand-oreore-certificate
+
+## How to use.
+
+```shell script
+docker-compose run --rm oreore /scripts/oreore.sh
+```
---
+++ scripts/oreore.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source /scripts/bin/file.sh
+source /scripts/bin/array.sh
+source /scripts/bin/message.sh
+
+source /scripts/conf/oreore.conf
+
+# Delete the last created Self-signed Certificate.
+# 前回作成されたオレオレ証明書(自己署名証明書)の削除。
+msg_info "Delete: last created"
+for filename in *; do
+  if ! contains "$filename" "${excludes[@]}"; then
+    msg_default "Delete: $filename"
+    delete "$filename"
+  fi
+done
+
+# Create Self-signed Certificate.
+# オレオレ証明書(自己署名証明書)の作成。
+msg_info "Create: server.key"
+openssl genrsa 2048 > server.key
+msg_info "Create: server.csr"
+openssl req -new -key server.key > server.csr
+msg_info "Create: server.crt"
+openssl x509 -days 3650 -req -signkey server.key < server.csr > server.crt
+
+# Complete.
+# 完了。
+msg_success "Complete!"
